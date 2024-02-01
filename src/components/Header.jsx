c1212d5dc224884f7a9fde0b99f45da51de81749
ronak raj <126279165+ronakraj00@users.noreply.github.com>
2024-02-02 00:22:47 +0530

    remove fix width on category content

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -40,7 +40,7 @@ function Header({cartLength}) {
                             Categories
                         </NavigationMenuTrigger>
                         <NavigationMenuContent>
-                            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                            <ul className="grid gap-3 p-4">
                                 <ListCategories
                                     categories={categories}
                                 ></ListCategories>
@@ -64,8 +64,8 @@ export function ListCategories({ categories }) {
                                 "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
                             )}
                         >
-                            <div className="text-sm font-medium leading-none">
-                                {category.toUpperCase()}
+                            <div className="text-sm font-medium leading-none capitalize">
+                                {category}
                             </div>
                         </Link>
                     </NavigationMenuLink>
