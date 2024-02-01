1d792a8d435da9ad16cb78fc1d276b2e37aae92a
ronak raj <126279165+ronakraj00@users.noreply.github.com>
2024-02-02 00:54:56 +0530

    layout cart cards

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -10,26 +10,30 @@ import { Link, useOutletContext } from "react-router-dom";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 
-import ShoppingImage from "../assets/images/shoppingImage.jpg"
+import ShoppingImage from "../assets/images/shoppingImage.jpg";
 
 function Cart() {
     const [cart, setCart] = useOutletContext();
     console.log("cart fro dfsajfal", cart);
 
-    return (
-      cart.length?
-        <main className="flex flex-col gap-3">
+    return cart.length ? (
+        <main className="flex flex-col gap-3 justify-center items-center mb-64">
+            <div className="text-xl font-mono">Your cart</div>
             {cart.map((item) => {
                 return (
                     <Card
                         key={item.id}
-                        className="w-max mx-6 flex flex-wrap items-center justify-center"
+                        className="w-max flex flex-wrap"
                     >
-                        <CardContent>
-                            <img src={item.image} alt="" className="w-16 p-4" />
-                        </CardContent>
-                        <div className="p-4">
-                            <CardTitle className="w-[20ch] my-2">
+                        <div className="p-2">
+                            <CardContent>
+                                <img
+                                    src={item.image}
+                                    alt=""
+                                    className="w-12 p-1"
+                                />
+                            </CardContent>
+                            <CardTitle className="w-[20ch] my-2 line-clamp-1">
                                 {item.title}
                             </CardTitle>
                             <CardDescription>
@@ -45,8 +49,7 @@ function Cart() {
                                 </p>
                             </CardDescription>
                         </div>
-                        <CardFooter className="p-4 justify-center flex flex-col gap-2">
-                            <Button>Remove</Button>
+                        <CardFooter className="flex flex-col justify-end gap-2 pb-2">
                             <Input
                                 min="1"
                                 max="5"
@@ -63,19 +66,21 @@ function Cart() {
                                     });
                                 }}
                             />
+                            <Button>Remove</Button>
                         </CardFooter>
                     </Card>
                 );
             })}
-            {cart.length?<CheckOut cart={cart} />:null}
-        </main>:
-        <EmptyCart/>
+            {cart.length ? <CheckOut cart={cart} /> : null}
+        </main>
+    ) : (
+        <EmptyCart />
     );
 }
 
 function CheckOut({ cart }) {
     return (
-        <div className="fixed bottom-0 p-8 border-2 w-full shadow-lg font-mono text-2xl flex justify-between">
+        <div className="fixed bg-white bottom-0 p-8 border-2 w-full shadow-lg font-mono text-2xl flex justify-between">
             <p>
                 Total Price{" "}
                 <span className="border-b-4 border-yellow-400">
@@ -92,19 +97,18 @@ function CheckOut({ cart }) {
     );
 }
 
-
 function EmptyCart() {
-  return (
-    <div className="pt-24 p-4 flex flex-col gap-4 justify-center items-center">
-      <p className="text-3xl font-mono font-bold">Your Cart is Empty</p>
-      <Link to={"/shop/all"}>
-      <Button>Go Shopping</Button>
-      </Link>
-      <div>
-        <img src={ShoppingImage} alt="" className="w-full"/>
-      </div>
-    </div>
-  )
+    return (
+        <div className="pt-24 p-4 flex flex-col gap-4 justify-center items-center">
+            <p className="text-3xl font-mono font-bold">Your Cart is Empty</p>
+            <Link to={"/shop/all"}>
+                <Button>Go Shopping</Button>
+            </Link>
+            <div>
+                <img src={ShoppingImage} alt="" className="w-full" />
+            </div>
+        </div>
+    );
 }
 
 export default Cart;
