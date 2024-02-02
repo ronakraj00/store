59708a1325ac7913b3aa9c7da25a9576fdc26412
ronak raj <126279165+ronakraj00@users.noreply.github.com>
2024-02-03 00:43:17 +0530

    change image order

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,11 +16,11 @@ function Hero() {
         "https://api.unsplash.com/photos/?client_id=KXB55fXOYigKnhXTI2ooOc9OJwxamNWipvNC80L8-nw";
 
     const banners = [
-        "https://images.unsplash.com/photo-1542291026-7eec264c27ff?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0ODczOTN8MHwxfGFsbHx8fHx8fHx8fDE3MDY1MzQ4Njd8&ixlib=rb-4.0.3&q=80&w=1080",
+        "https://images.unsplash.com/photo-1523275335684-37898b6baf30?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0ODczOTN8MHwxfGFsbHx8fHx8fHx8fDE3MDY1MzQ1MzR8&ixlib=rb-4.0.3&q=80&w=1080",
         "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0ODczOTN8MHwxfGFsbHx8fHx8fHx8fDE3MDY1MzQ5Mjh8&ixlib=rb-4.0.3&q=80&w=1080",
         "https://images.unsplash.com/photo-1511556820780-d912e42b4980?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0ODczOTN8MHwxfGFsbHx8fHx8fHx8fDE3MDY1MzQ2OTV8&ixlib=rb-4.0.3&q=80&w=1080",
         "https://images.unsplash.com/photo-1571781926291-c477ebfd024b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0ODczOTN8MHwxfGFsbHx8fHx8fHx8fDE3MDY1MzQ2NTR8&ixlib=rb-4.0.3&q=80&w=1080",
-        "https://images.unsplash.com/photo-1523275335684-37898b6baf30?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0ODczOTN8MHwxfGFsbHx8fHx8fHx8fDE3MDY1MzQ1MzR8&ixlib=rb-4.0.3&q=80&w=1080",
+        "https://images.unsplash.com/photo-1542291026-7eec264c27ff?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0ODczOTN8MHwxfGFsbHx8fHx8fHx8fDE3MDY1MzQ4Njd8&ixlib=rb-4.0.3&q=80&w=1080",
     ];
 
     return (
